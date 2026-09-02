776420c5df4b93b1895b4b2a35b642bd3224af2e
soubabood15 <soubabood15@gmail.com>
2026-09-02 18:01:34 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "newtel-ebook-v200";
-const META_CACHE_NAME = "newtel-ebook-meta-v200";
+const CACHE_NAME = "newtel-ebook-v201";
+const META_CACHE_NAME = "newtel-ebook-meta-v201";
 const SHELL_TTL = 24 * 60 * 60 * 1000;
 
 // Only the entry shell is preloaded. Every other project is cached lazily
